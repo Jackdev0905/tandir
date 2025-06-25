3c812d75d4606e69fab0291c873ff91ee027305f
Jackdev0905 <abdurahimovjavohir09@gmail.com>
2025-06-25 13:29:21 +0900

    feat: integration of images uploader

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import adminController from "./controllers/admin.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 const routerAdmin = express.Router();
 
@@ -23,7 +24,7 @@ routerAdmin.get(
   adminController.verifyAdmin,
   productController.getAllProducts
 );
-routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/create",makeUploader("products").array("productImages", 5), productController.createNewProduct);
 routerAdmin.post("/product/:id", productController.updateChosenProduct);
 
 export default routerAdmin;
